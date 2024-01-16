dd105d80bc4e07db7890a54499bb84e8ad071eab
Deyci Reubey Wynne <reubeycw@gmail.com>
2024-01-16 18:02:21 +0000

    Added basic tests

--- CheckoutKata.Test/UnitTest1.cs
+++ CheckoutKata.Test/UnitTest1.cs
@@ -1,10 +1,57 @@
 namespace CheckoutKata.Test;
 
+using checkoutkata;
+
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Checkout_GetTotalPrice_NoItems_ReturnsZero()
     {
+        // Arrange
+        var checkout = new ConcreteCheckout();
 
+        // Act
+        var result = checkout.GetTotalPrice();
+
+        // Assert
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("A", 50)]
+    [InlineData("B", 30)]
+    [InlineData("C", 20)]
+    [InlineData("D", 15)]
+    public void Checkout_GetTotalPrice_OneItem_ReturnsCorrectPrice(string item, int expectedPrice)
+    {
+        // Arrange
+        var checkout = new ConcreteCheckout();
+
+        // Act
+        checkout.Scan(item);
+        var result = checkout.GetTotalPrice();
+
+        // Assert
+        Assert.Equal(expectedPrice, result);
+    }
+
+    [Theory]
+    [InlineData((string[])(["A", "B"]), 80)]
+    [InlineData((string[])(["A", "B", "C"]), 100)]
+    [InlineData((string[])(["A", "B", "C", "D"]), 115)]
+    public void Checkout_GetTotalPrice_MultipleItems_ReturnsCorrectPrice(string[] items, int expectedPrice)
+    {
+        // Arrange
+        var checkout = new ConcreteCheckout();
+
+        // Act
+        foreach (var item in items)
+        {
+            checkout.Scan(item);
+        }
+        var result = checkout.GetTotalPrice();
+
+        // Assert
+        Assert.Equal(expectedPrice, result);
     }
 }
